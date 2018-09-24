86e7be910633f4e3cde23e2911a7304bc3e419a5
miquelnogue <35957706+miquelnogue@users.noreply.github.com>
2018-09-24 18:19:20 +0200

    Practica 1 CAES

--- lab/junithelloworld/src/test/java/edu/udg/caes/ExercicesIntroTestingTest.java
+++ lab/junithelloworld/src/test/java/edu/udg/caes/ExercicesIntroTestingTest.java
@@ -29,4 +29,31 @@ public class ExercicesIntroTestingTest {
         final int result = ExercicesIntroTesting.oddOrPos(new int []{-3, -2, 0, 1, 4});
         assertEquals(result, 3);
     }
+
+
+    // PARAMETRES QUE FAN QUE CODI ERRONI DONI CORRECTE
+
+    @Test
+    public void testFindLast1(){
+        final int result = ExercicesIntroTesting.findLast(new int []{2, 3, 5}, 3);
+        assertEquals(result, 1);
+    }
+
+    @Test
+    public void testLastZero1(){
+        final int result = ExercicesIntroTesting.lastZero(new int []{1, 1, 0});
+        assertEquals(result, 2);
+    }
+
+    @Test
+    public void testCountPositive1(){
+        final int result = ExercicesIntroTesting.countPositive(new int []{-4, 2, -1, 2});
+        assertEquals(result, 2);
+    }
+
+    @Test
+    public void testOddOrPos1(){
+        final int result = ExercicesIntroTesting.oddOrPos(new int []{1});
+        assertEquals(result, 1);
+    }
 }
